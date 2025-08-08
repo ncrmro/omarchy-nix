41fc7960e403168f13e584111eed93437d3a951d
Nicholas Romero <ncrmro@gmail.com>
2025-08-08 02:09:21 -0500

    fix: importing omarchy defaults

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -22,6 +22,7 @@ inputs: {
     else null;
 in {
   imports = [
+    (import ./omarchy-defaults.nix)
     (import ./hyprland.nix inputs)
     (import ./hyprlock.nix inputs)
     (import ./hyprpaper.nix)
