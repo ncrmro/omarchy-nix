10c4e8c69e22590535c377a3997f88bfa449e4ad
Nicholas Romero <ncrmro@gmail.com>
2025-08-12 03:30:08 -0500

    chore: remove hyperpaper

--- modules/home-manager/hyprpaper.nix
+++ modules/home-manager/hyprpaper.nix
@@ -2,15 +2,8 @@
   config,
   pkgs,
   ...
-}: let
-  selected_wallpaper_path = (import ../../lib/selected-wallpaper.nix config).wallpaper_path;
-in {
-  home.file = {
-    "Pictures/Wallpapers" = {
-      source = ../../config/themes/wallpapers;
-      recursive = true;
-    };
-  };
+} {
+
   services.hyprpaper = {
     enable = true;
     settings = {
