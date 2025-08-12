440f2fc1b4058429c09fb137fa8c6085fce651ee
Nicholas Romero <ncrmro@gmail.com>
2025-08-12 03:30:54 -0500

    chore: try setting uwsm env

--- modules/home-manager/hyprland/envs.nix
+++ modules/home-manager/hyprland/envs.nix
@@ -13,6 +13,41 @@
     "__GLX_VENDOR_LIBRARY_NAME,nvidia"
   ];
 in {
+  home.file."${config.xdg.configHome}/uwsm/env".text = ''
+        "XCURSOR_SIZE,24"
+        "HYPRCURSOR_SIZE,24"
+
+        # Cursor theme
+        "XCURSOR_THEME,Adwaita"
+        "HYPRCURSOR_THEME,Adwaita"
+
+        # Force all apps to use Wayland
+        "GDK_BACKEND,wayland"
+        "QT_QPA_PLATFORM,wayland"
+        "QT_STYLE_OVERRIDE,kvantum"
+        "SDL_VIDEODRIVER,wayland"
+        "MOZ_ENABLE_WAYLAND,1"
+        "ELECTRON_OZONE_PLATFORM_HINT,wayland"
+        "OZONE_PLATFORM,wayland"
+
+        # Make Chromium use XCompose and all Wayland
+        "CHROMIUM_FLAGS,\"--enable-features=UseOzonePlatform --ozone-platform=wayland --gtk-version=4\""
+
+        # Make .desktop files available for wofi
+        "XDG_DATA_DIRS,$XDG_DATA_DIRS:$HOME/.nix-profile/share:/nix/var/nix/profiles/default/share"
+
+        # Use XCompose file
+        "XCOMPOSEFILE,~/.XCompose"
+        "EDITOR,nvim"
+        
+        # GTK theme
+        "GTK_THEME,${if cfg.theme == "generated_light" then "Adwaita" else "Adwaita:dark"}"
+  '';
+  home.file."${config.xdg.configHome}/uwsm/env-hyprland".text = ''
+    "HYPRCURSOR_SIZE,24"
+    "HYPRCURSOR_THEME,Adwaita"
+  '';
+
   wayland.windowManager.hyprland.settings = {
     # Environment variables
     env =
